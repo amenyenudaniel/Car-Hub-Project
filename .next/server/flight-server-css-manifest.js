53ac9ccb749e3262b85e00594f73ed833ab4e639
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-07-29 14:01:52 +0000

    final commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Daniel Amenyenu\\Desktop\\carent\\app\\layout.tsx": [
+      "C:\\Users\\Daniel Amenyenu\\Desktop\\carent\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Daniel Amenyenu\\Desktop\\carent\\app\\page": [
+      "C:\\Users\\Daniel Amenyenu\\Desktop\\carent\\app\\globals.css"
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Car Hub Application
+
+![Alt text](<rent car web.png>)
+
+## This Application was build using the following;
+
+### Next Js
+
+### Tailwind Css
+
+### Rapid API
+
+### Typescript
